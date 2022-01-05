614f49910b01c4bb512e89b6a44e97e802543d46
Marijana Maric <marijanamaric@Marijanas-MacBook-Pro.local>
2022-01-05 13:08:37 +0100

    test

--- src/pages/tables/DataGrid.js
+++ src/pages/tables/DataGrid.js
@@ -68,29 +68,6 @@ const CssTextField = styled(TextField, {
   },
 }));
 
-const CssSelect = styled(Select, {
-  shouldForwardProp: (props) => props !== "focusColor",
-})((p) => ({
-  // input label when focused
-  "& label.Mui-focused": {
-    color: p.focusColor,
-  },
-  // focused color for input with variant='standard'
-  "& .MuiInput-underline:after": {
-    borderBottomColor: p.focusColor,
-  },
-  // focused color for input with variant='filled'
-  "& .MuiFilledInput-underline:after": {
-    borderBottomColor: p.focusColor,
-  },
-  // focused color for input with variant='outlined'
-  "& .MuiOutlinedInput-root": {
-    "&.Mui-focused fieldset": {
-      borderColor: "black",
-    },
-  },
-}));
-
 const useStyles = makeStyles({
   root: {
     width: 200,
