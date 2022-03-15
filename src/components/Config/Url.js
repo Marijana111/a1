05eb8849b5f4785d9449815f90bbdb141afadad9
Goran Bralo <goranbralo@gmail.com>
2022-03-15 12:33:49 +0100

    add status request mwthod

--- src/components/Config/Url.js
+++ src/components/Config/Url.js
@@ -1,9 +1,13 @@
 export let a1Url =
   "https://056d00c1-0acf-4ee4-8fcb-00aff455555a.mock.pstmn.io/api/v1";
-// export let a1Url = "http://b2b-ws-persistence.apps.tos.vipnet.hr/api/v1";
+
+export let a1processorURL =
+  "http://a560d6f4-56bb-49b7-a524-4021c4a7580e.mock.pstmn.io/api/v1/business";
 
 export const businessPersistenceURL = `${a1Url}/business/persistence`;
 export const requestsURL = `${a1Url}/business/persistence/orders`;
 export const faultOrdersURL = `${a1Url}/business/persistence/faultOrders`;
 export const reportOrdersURL = `${a1Url}/business/persistence/reportOrders`;
 export const homeURL = `${a1Url}/business/persistence`;
+export const orderStatusURL = `${a1processorURL}/orderStatus`;
+export const faultStatusURL = `${a1processorURL}/faultStatus`;
